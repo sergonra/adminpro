9e9cf42b3cfb94aca3b3f2e5849fb45a6ddb65af
Sergonra <sergonra@gmail.com>
2024-03-17 14:03:20 +0100

    subida del routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,31 @@
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router'
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
-import { PagesComponent } from './pages/pages.component';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes =[
-  {
-    path: '',
-    component: PagesComponent,
-    children:[
-      {path:'dasboard', component: DashboardComponent},
-      {path:'progress', component: ProgressComponent},
-      {path:'grafica1', component: Grafica1Component},
-      {path:'', redirectTo: '/dasboard',pathMatch: 'full'},
-    ]
-  },
+// Modulos
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
-  {path:'', component: PagesComponent},
-  {path:'login', component: LoginComponent},
-  {path:'register', component: RegisterComponent},
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
 
 
-  {path:'**', component: NotpagefoundComponent},
+const routes: Routes = [
+
+  // path: '/dashboard' PagesRouting
+  // path: '/auth' AuthRouting
+  // path: '/medicos' MedicosRouting
+  // path: '/compras' ComprasRouting
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NotpagefoundComponent },
 ];
 
 
+
 @NgModule({
-  declarations: [],
   imports: [
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
-  exports: [RouterModule]
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
